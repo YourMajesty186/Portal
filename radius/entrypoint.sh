25422434f16a67d795cf5f85d283c57f091c091a
keysyayst <keysyayesantisafaat@gmail>
2026-08-12 08:24:05 +0700

    Update Security

--- radius/entrypoint.sh
+++ radius/entrypoint.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
-# Jika konfigurasi belum ada, copy default dari image
 if [ ! -f /etc/freeradius/radiusd.conf ]; then
     echo "Copying default FreeRADIUS configuration..."
     cp -r /etc/freeradius.default/* /etc/freeradius/
 fi
 
-# Jalankan FreeRADIUS
 exec freeradius -f -X
